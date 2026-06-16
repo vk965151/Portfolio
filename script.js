9b4003684c816f046e6c4d64507dcfebd77e488b
Vishal Kashyap <vk965151@gmail.com>
2026-06-16 23:53:08 +0530

    add another tabe

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ project1.addEventListener("click", () => {
      window.open("https://amazone-project-lemon.vercel.app/", "_blank");
 });
 
+const project2 = document.querySelector(".project-2");
+project2.addEventListener("click", () => {
+     window.open("https://amazone-project-lemon.vercel.app/", "_blank");
+});
+
