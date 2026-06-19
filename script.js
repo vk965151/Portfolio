c8d95d1951e9be2a8135b34d9bf2d62a66c6da91
Vishal Kashyap <vk965151@gmail.com>
2026-06-19 23:32:42 +0530

    make it responsive

--- script.js
+++ script.js
@@ -8,5 +8,15 @@ project3.addEventListener("click", () => {
      window.open("https://rock-game-vishal.vercel.app/", "_blank");
 });
 
+const github2 = document.querySelector(".logo-1");
+github2.addEventListener("click", () => {
+     window.open("https://github.com/vk965151", "_blank");
+});
+
+const linkdin2 = document.querySelector(".logo-2");
+linkdin2.addEventListener("click", () => {
+     window.open("https://www.linkedin.com/in/vishal-kashyap-9812b2348/", "_blank");
+});
+
 
 
